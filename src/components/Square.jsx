aecb29c5fe78b87626a670d981507402e804bfba
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2022-01-13 17:18:05 +0530

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {this.props.value}
+            </button>
+        );
+    }
+}
+export default Square;
